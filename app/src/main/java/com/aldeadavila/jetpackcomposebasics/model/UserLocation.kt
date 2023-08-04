5bb4740aa20b2639a27489d1ef7a633ac2618cc0
Francisco Jose Marquina Garcia <aldeadavila@gmail.com>
2023-08-04 21:18:26 +0200

    model rest and add retrofit

---
+++ app/src/main/java/com/aldeadavila/jetpackcomposebasics/model/UserLocation.kt
@@ -0,0 +1,6 @@
+package com.aldeadavila.jetpackcomposebasics.model
+
+data class UserLocation (
+    val city: String,
+    val state: String
+    )
---
+++ app/src/main/java/com/aldeadavila/jetpackcomposebasics/model/UserPicture.kt
@@ -0,0 +1,7 @@
+package com.aldeadavila.jetpackcomposebasics.model
+
+data class UserPicture(
+    val thumbnail: String,
+    val medium: String,
+    val large: String
+)
