89ff6f3bbfcd33449431010f68800be72266c2f0
Francisco Jose Marquina Garcia <aldeadavila@gmail.com>
2023-08-04 20:35:14 +0200

    add dagger hilt

--- app/src/main/java/com/aldeadavila/jetpackcomposebasics/MainActivity2.kt
+++ app/src/main/java/com/aldeadavila/jetpackcomposebasics/MainActivity2.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.aldeadavila.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity2 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/aldeadavila/jetpackcomposebasics/MyApplication.kt
@@ -0,0 +1,7 @@
+package com.aldeadavila.jetpackcomposebasics
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() { }
